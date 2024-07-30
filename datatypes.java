c2d448e42374f8a7255ba3a648df566f0b5ef9ef
HardikWagh <161699531+HardikWagh@users.noreply.github.com>
2024-07-30 13:32:18 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,15 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        int integerVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        String stringVar = "Hello, Java!";
+        boolean booleanVar = true;
+
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Char: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + booleanVar);
+    }
+}
